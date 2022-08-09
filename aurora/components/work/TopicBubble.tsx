ee88137632e0decf48abb2a78115a3504b8310d9
Fred <iblueman260@gmail.com>
2022-08-09 20:02:02 +0300

    Add TopicBubble component & Tech types

--- aurora/components/work/TopicBubble.tsx
+++ aurora/components/work/TopicBubble.tsx
@@ -1,10 +1,12 @@
+import config from '../../config.json';
 import styles from '../../styles/TopicBubble.module.css';
-const techTypes = new Map<string, string>()
-    .set('react', 'front')
-    .set('Spring-Boot', 'back');
 
 export default function TopicBubble({ topic }: { topic: string }) {
-    const topicType = techTypes.get(topic);
+    // ToDo: Optimize the map so that map is created only once at the parent level and passed down to TopicBubble
+    // TODO: remove the any identifier
+    const list: any = config.TECH_TYPES;
+    const techTypes = new Map<string, string>(list);
 
+    const topicType = techTypes.get(topic);
     return <div className={`${styles.topicBubble} ${topicType}`}>{topic}</div>;
 }
