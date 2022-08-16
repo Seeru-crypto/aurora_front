bcddaf243d076e7b2fe75852d3bb524890782d67
Risto Kitsing <risto.kitsing@outlook.com>
2022-08-16 20:12:19 +0300

    Added increment example (with Redux Toolkit)

--- aurora/src/components/about/About.tsx
+++ aurora/src/components/about/About.tsx
@@ -1,3 +1,5 @@
+import { decrement, increment } from "../../state/appSlice";
+import { RootState, useAppDispatch, useAppSelector } from "../../state/store";
 import styles from "../../styles/About.module.css";
 export interface AboutInterface {
   main_text: string[];
@@ -8,9 +10,15 @@ export interface AboutInterface {
   contact_email: string;
 }
 const About = () => {
+  const activeCount = useAppSelector((state: RootState) => state.counter.value);
+  const dispatch = useAppDispatch();
+
   return (
     <section id="about" className={styles.container}>
       <h1>About</h1>
+      <p>Counter:&nbsp;{activeCount}</p>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
     </section>
   );
 };
--- aurora/src/pages/_app.tsx
+++ aurora/src/pages/_app.tsx
@@ -1,14 +1,18 @@
-import type { AppProps } from 'next/app';
-import Layout from '../components/layout/layout';
-import '../styles/globals.css';
-import '../styles/topic.css';
+import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import Layout from "../components/layout/layout";
+import { store } from "../state/store";
+import "../styles/globals.css";
+import "../styles/topic.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return (
-        <Layout>
-            <Component {...pageProps} />
-        </Layout>
-    );
+  return (
+    <Provider store={store}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
+  );
 }
 
 export default MyApp;
