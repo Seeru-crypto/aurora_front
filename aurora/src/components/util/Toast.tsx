0372987ab8bc5e2e172b24f0b69db285151148a4
Fred <iblueman260@gmail.com>
2022-08-23 22:01:04 +0300

    refactor toast

--- aurora/src/components/util/Toast.tsx
+++ aurora/src/components/util/Toast.tsx
@@ -4,34 +4,72 @@ import styled from 'styled-components';
 const Toast = () => {
     return (
         <StyledToast>
-            <span className="toastIcon">i</span>
-            <span>Saved to clipboard!</span>
+            <div className="snackbar">copied to clipboard</div>
         </StyledToast>
     );
 };
 
 const StyledToast = styled.div`
-    position: fixed;
-    top: 4rem;
-    right: 1rem;
-    z-index: 9999;
-    display: inline-flex;
-    padding: 10px 20px;
-    background: var(--primary-color);
-    color: var(--button-text);
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
-    border-radius: 15px;
+    .snackbar {
+        visibility: visible; /* Show the snackbar */
+        /* Add animation: Take 0.5 seconds to fade in and out the snackbar.
+  However, delay the fade out process for 2.5 seconds */
+        -webkit-animation: fadein 0.5s, fadeout 0.5s 2.5s;
+        animation: fadein 0.5s, fadeout 0.5s 2.5s;
+        min-width: 250px; /* Set a default minimum width */
+        margin-left: -125px; /* Divide value of min-width by 2 */
+        background-color: #333; /* Black background color */
+        color: #fff; /* White text color */
+        text-align: center; /* Centered text */
+        border-radius: 2px; /* Rounded borders */
+        padding: 16px; /* Padding */
+        position: fixed; /* Sit on top of the screen */
+        z-index: 1; /* Add a z-index if needed */
+        left: 50%; /* Center the snackbar */
+        bottom: 30px; /* 30px from the bottom */
+    }
+    @-webkit-keyframes fadein {
+        from {
+            bottom: 0;
+            opacity: 0;
+        }
+        to {
+            bottom: 30px;
+            opacity: 1;
+        }
+    }
 
-    .toastIcon {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        width: 20px;
-        height: 20px;
-        margin-right: 10px;
-        background: #2196f3;
-        color: #fff;
-        border-radius: 50%;
+    @keyframes fadein {
+        from {
+            bottom: 0;
+            opacity: 0;
+        }
+        to {
+            bottom: 30px;
+            opacity: 1;
+        }
+    }
+
+    @-webkit-keyframes fadeout {
+        from {
+            bottom: 30px;
+            opacity: 1;
+        }
+        to {
+            bottom: 0;
+            opacity: 0;
+        }
+    }
+
+    @keyframes fadeout {
+        from {
+            bottom: 30px;
+            opacity: 1;
+        }
+        to {
+            bottom: 0;
+            opacity: 0;
+        }
     }
 `;
 export default Toast;
