60a345a344c84ed4ff331291f8bff4415420f113
Fred-Andree Oja <fred-andree.oja@netgroup.com>
2022-08-26 10:48:48 +0300

    Add time based greeting to LandingPage.tsx

--- aurora/src/components/landing/LandingPage.tsx
+++ aurora/src/components/landing/LandingPage.tsx
@@ -1,9 +1,31 @@
 import Image from 'next/image';
-import React, { ForwardedRef } from 'react';
+import React, {ForwardedRef, useEffect, useState} from 'react';
 import styled from 'styled-components';
+import data from "../../data.json"
 
 const LandingPage = React.forwardRef(
     (_props, ref: ForwardedRef<HTMLElement>) => {
+        const [greetingMessage, setGreetingMessage] = useState("");
+
+        useEffect(() => {
+            const sysTimeHour = new Date().getHours();
+            switch (true){
+                case (sysTimeHour < 6):
+                    setGreetingMessage(data.GREETING_EVENING);
+                    break;
+                case (7 <= sysTimeHour && sysTimeHour <= 11 ):
+                    setGreetingMessage(data.GREETING_MORNING);
+                    break;
+                case (12 <= sysTimeHour && sysTimeHour < 20):
+                    setGreetingMessage(data.GREETING_DAY);
+                    break;
+                case (20 <= sysTimeHour):
+                    setGreetingMessage(data.GREETING_EVENING);
+                    break;
+                default: setGreetingMessage("Howdy");
+            }
+        }, [])
+
         return (
             <LandingStyle id="home" ref={ref} className={'container'}>
                 <div className={'right'}>
@@ -16,7 +38,8 @@ const LandingPage = React.forwardRef(
                 </div>
 
                 <div className={'left'}>
-                    <h1>Hi My name is Fred</h1>
+                    <h1>{greetingMessage}</h1>
+                    <h1>My name is Fred</h1>
                     <p>I do stuff for the interwebz</p>
                 </div>
             </LandingStyle>
