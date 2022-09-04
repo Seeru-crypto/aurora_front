19dacaf387a591c5c667cdb076cbd5856ae9b83a
Fred <iblueman260@gmail.com>
2022-09-04 16:36:30 +0300

    fix hydration error, add basic archive table

--- aurora/src/components/work/Archive.tsx
+++ aurora/src/components/work/Archive.tsx
@@ -1,22 +1,63 @@
+import styled from 'styled-components';
+import config from '../../config.json';
 import { ProjectInterface } from '../../lib/load-data';
-import styled from "styled-components";
 
 export default function Archive({
     projects,
 }: {
     projects: ProjectInterface[];
 }) {
+    const { GITHUB_URL } = config;
     return (
         <ArchiveStyle>
             <h3>Arhive list!</h3>
-            {projects.map((project, index) => (
-                <div key={index}>
-                    <p>{`${index+1}. ${project.project_name}`}</p>
-                </div>
-            ))}
+            <table className="table">
+                <tbody>
+                    <tr className="tableHeader">
+                        <th>Year</th>
+                        <th>Name</th>
+                        <th>Type</th>
+                        <th>Topics</th>
+                        <th>Links</th>
+                    </tr>
+                    {projects.map((project, index) => (
+                        <tr className="tableRow" key={index}>
+                            <td className="tableContent">{index + 1}</td>
+                            <td className="tableContent">
+                                {project.project_name}
+                            </td>
+                            <td className="tableContent">
+                                {project.project_type}
+                            </td>
+                            <td className="tableContent">
+                                {project.topics?.map((topic) => (
+                                    <span key={topic}>{`${topic}, `}</span>
+                                ))}
+                            </td>
+                            <td>
+                                {project.repo_name && (
+                                    <a
+                                        href={`${GITHUB_URL}/${project.repo_name}`}
+                                    >
+                                        repo_link
+                                    </a>
+                                )}
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </ArchiveStyle>
     );
 }
 const ArchiveStyle = styled.div`
     height: 50vh;
-`
+    padding: 2rem 0;
+    .table {
+        width: 100%;
+        border: 1px solid black;
+    }
+    .tableContent {
+        border: 1px solid orange;
+    }
+`;
--- aurora/src/lib/load-data.tsx
+++ aurora/src/lib/load-data.tsx
@@ -1,97 +1,96 @@
-import fsPromises from "fs/promises";
-import path from "path";
-import config from "../config.json";
+import fsPromises from 'fs/promises';
+import path from 'path';
+import config from '../config.json';
 
 export interface ProjectJsonInterface {
-  repo_name: string;
-  project_name: string;
-  picture_url: string;
-  project_type: ProjectTypes;
-  showcase: boolean;
-  topics?: string[];
-  description?: string;
+    repo_name: string;
+    project_name: string;
+    picture_url: string;
+    project_type: ProjectTypes;
+    showcase: boolean;
+    topics?: string[];
+    description?: string;
+    dateFinished?: string;
 }
 
 export interface WorkProps {
-  projects: ProjectInterface[];
-  techTypes: TechTypes;
+    projects: ProjectInterface[];
+    techTypes: TechTypes;
 }
 
 export type TechTypes = Map<string, string>;
 
-export type ProjectTypes = ["personal", "freelance", "main"];
+export type ProjectTypes = ['personal', 'freelance', 'main'];
 
 export interface ProjectInterface extends ProjectJsonInterface {
-  description?: string;
-  created_at?: string;
-  updated_at?: string;
-  homepage?: string;
-  topics?: string[];
+    description?: string;
+    created_at?: string;
+    updated_at?: string;
+    homepage?: string;
+    topics?: string[];
 }
-
 export interface ProjectGitRepoInterface {
-  description: string;
-  created_at: string;
-  updated_at: string;
-  homepage: string;
-  topics: string[];
+    description: string;
+    created_at: string;
+    updated_at: string;
+    homepage: string;
+    topics: string[];
 }
 
 // TODO: Check, if MacOS is different from Windows here with process.cwd()
 export async function loadLocalData() {
-  const filePath = path.join(process.cwd(), "src/data.json");
-  const jsonData = await fsPromises.readFile(filePath);
-  return JSON.parse(jsonData.toString());
+    const filePath = path.join(process.cwd(), 'src/data.json');
+    const jsonData = await fsPromises.readFile(filePath);
+    return JSON.parse(jsonData.toString());
 }
 
 export async function getThirdPartyData(
-  url: string,
-  token?: string
+    url: string,
+    token?: string
 ): Promise<any> {
-  let response;
-  await fetch(url, {
-    method: "GET",
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  })
-    .then((requestPromise) => {
-      if (!requestPromise.ok)
-        throw new Error(`HTTP Error: ${requestPromise.status}`);
-      return requestPromise.json();
+    let response;
+    await fetch(url, {
+        method: 'GET',
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
     })
-    .then((data) => {
-      response = data;
-    })
-    .catch((error) => {
-      console.error(`${error}, for ${url}`)
-    });
-  return response;
-
+        .then((requestPromise) => {
+            if (!requestPromise.ok)
+                throw new Error(`HTTP Error: ${requestPromise.status}`);
+            return requestPromise.json();
+        })
+        .then((data) => {
+            response = data;
+        })
+        .catch((error) => {
+            console.error(`${error}, for ${url}`);
+        });
+    return response;
 }
 
 export async function mergeGitProjectData(
-  projects: ProjectJsonInterface[],
-  token: string | undefined
+    projects: ProjectJsonInterface[],
+    token: string | undefined
 ): Promise<ProjectInterface[]> {
-  return Promise.all(
-    projects.map(async (project) => {
-      if (project.repo_name === "") return { ...project };
+    return Promise.all(
+        projects.map(async (project) => {
+            if (project.repo_name === '') return { ...project };
 
-      const gitData: ProjectGitRepoInterface = await getThirdPartyData(
-        config.GIT_REPO_DATA_URL + project.repo_name,
-        token
-      )
-      if (!gitData) return { ...project };
+            const gitData: ProjectGitRepoInterface = await getThirdPartyData(
+                config.GIT_REPO_DATA_URL + project.repo_name,
+                token
+            );
+            if (!gitData) return { ...project };
 
-      return {
-        ...project,
-        description: gitData.description,
-        created_at: gitData.created_at,
-        updated_at: gitData.updated_at,
-        homepage: gitData.homepage,
-        topics: gitData.topics,
-      };
-    })
-  );
+            return {
+                ...project,
+                description: gitData.description,
+                created_at: gitData.created_at,
+                updated_at: gitData.updated_at,
+                homepage: gitData.homepage,
+                topics: gitData.topics,
+            };
+        })
+    );
 }
