3680ed772eaa54ded0e2f7306bf042be5898944c
Fred-Andree Oja <fred-andree.oja@netgroup.com>
2022-09-06 16:04:08 +0300

    enable Image unoptimization

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@ module.exports = {
   reactStrictMode: true,
   distDir: 'build',
   swcMinify: true,
+  experimental: {
+    images: {
+      unoptimized: true,
+    },
+  },
   env: {
     REVALIDATE_VALUE: 7 * 86400,
   },
