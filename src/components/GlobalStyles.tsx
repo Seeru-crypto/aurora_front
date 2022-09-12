abaee11c235d3e2aa9227d8a182a7008ccf14004
Risto Kitsing <risto.kitsing@outlook.com>
2022-09-12 21:19:04 +0300

    Modified GlobalStyles

--- src/components/GlobalStyles.tsx
+++ src/components/GlobalStyles.tsx
@@ -1,14 +1,39 @@
-import { createGlobalStyle } from 'styled-components';
-import { ActiveTheme } from '../state/themeSlice';
+import { createGlobalStyle } from "styled-components";
+import { ActiveTheme } from "../state/themeSlice";
 
 export default createGlobalStyle<{ activeTheme: string }>`
+:root {
+    --bkg-color: #fffffe;
+    --primary-color: #6246ea;
+    --primary-color-300: hsla(250, 80%, 60%, 0.353);
+
+    --secondary-color: #d1d1e9;
+    --button-text: #fffffe;
+    --paragraph: #2b2c34;
+    --header-color: #2b2c34;
+    --hero-section-bkg: #444;
+    --icon-highlight-border: purple;
+    --btn-hover-bkg: purple;
+    --nav-main-btn-color: purple;
+    --nav-active-color: purple;
+    --box-shadow: #2b2c34;
+    --transition: all 0.3s ease-in-out;
+    --front-topic-color: red;
+    --back-topic-color: green;
+    --other-topic-color: purple;
+    --db-topic-color: blue;
+    --table-even: #cbd5e1; /*Slate 300  */
+    --table-uneven: #f1f5f9; /* Slate 100 */
+}
+
 html {  
   scroll-behavior: smooth;
   scroll-padding-top: 5vmax;
 }
 
 body {
-  background-color: ${(props) => props.activeTheme === ActiveTheme.NIGHT && props.theme.background};
+  background-color: ${(props) =>
+    props.activeTheme === ActiveTheme.NIGHT && props.theme.background};
   color: ${(props) => props.theme.text};
   font-family: 'Montserrat', sans-serif;
   margin: 0;
