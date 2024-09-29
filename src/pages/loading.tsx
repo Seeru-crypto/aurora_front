551f057c39553e054a881875d84e2d64ce00159e
Seeru <puupuha@gmail.com>
2024-09-29 14:16:00 +0300

    feat: improve loading screen

--- src/pages/loading.tsx
+++ src/pages/loading.tsx
@@ -1,8 +1,24 @@
+import styled from 'styled-components';
+
 export default function Loading() {
   // You can add any UI inside Loading, including a Skeleton.
   return (
-    <div>
-      <h1>LOADING</h1>
-    </div>
+    <LoadingStyle>
+    </LoadingStyle>
   )
 }
+
+const LoadingStyle = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+
+  background-color: #EEEEEE; // GRAY 0
+  color: #e2e8f0; // gray 200
+  
+  body {
+    background-color: #EEEEEE; // GRAY 0
+  }
+`;
